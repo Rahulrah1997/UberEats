45b2474c651a10419b05d56ae1e89fbb3f875c78
Rahul <97379229+Rahulrah1997@users.noreply.github.com>
2026-05-23 12:24:59 +0530

    Update backend.py

--- App/backend.py
+++ App/backend.py
@@ -1,3 +1,29 @@
-import pandas
+import streamlit as st
+import sqlite3
+from ResDataExtract import Data_clean
 
-a =1
+class QAPage():
+    
+    def ResData(self):
+        cleaned_Data = Data_clean.Res_DataExtract()
+        conn = sqlite3.connect('ubereats.db')
+        cursor = conn.cursor()
+        try:
+            cursor.execute("Begin")
+            cursor.execute('''CREATE TABLE IF NOT EXISTS restaurant_details(
+                        id INTEGER PRIMARY KEY, rest_name TEXT, online_order VARCHAR(3), 
+                        book_table VARCHAR(3), rate REAL, votes REAL, phone TEXT,location TEXT,rest_type TEXT,
+                        dish_liked TEXT, cuisines TEXT, approx_cost TEXT, listed_in_type TEXT, listed_in_city TEXT, country_code VARCHAR(3))''')
+
+            cleaned_Data.to_sql('restaurant_details',conn,if_exists='replace',index=False)
+            
+            conn.commit()
+
+        except sqlite3.Error as err:
+            conn.rollback()
+            print(err)
+        return self.conn
+
+
+    
+    
